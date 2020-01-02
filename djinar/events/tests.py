c2f82d43084fd3972c6cba383f56ddc345dbfd7b
Bruno Fosados <bruno.fosados@gmail.com>
2020-01-02 01:00:37 -0600

    Add missed retrieve event test

--- djinar/events/tests.py
+++ djinar/events/tests.py
@@ -81,6 +81,125 @@ class EventTestCase(APITestCase):
         )
         event.delete()
 
+    def test_event_retrieve(self):
+        """[summary]
+        Test event retrieve endpoint.
+        [description]
+        """
+        event_title = "Test Event for Retrieve"
+        event_data = self.event_data
+        event_data.update({
+            "title": event_title,
+            "attendants": [{"pid": str(self.contact.pid)}],
+        })
+        endpoint = reverse("events:create")
+        resp = self.client.post(endpoint, event_data, format='json')
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_201_CREATED,
+            msg=f"Event not created status code: {resp.status_code}",
+        )
+        event = Event.objects.get(title=event_title)
+        endpoint = reverse("events:items")
+
+        event_search_title = {
+            "search": event.title
+        }
+        resp = self.client.get(endpoint, event_search_title, format="json")
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_200_OK,
+            msg=f"Event not retrieved by title, status_code {resp.status_code}",
+        )
+        self.assertTrue(
+            resp.json()["results"][0],
+            msg="Event not retrieved by title",
+        )
+
+        event_search_attendants_name = {
+            "search": event.attendants.first().name
+        }
+        resp = self.client.get(endpoint, event_search_attendants_name, format="json")
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_200_OK,
+            msg=f"Event not retrieved by attendants_name, status_code {resp.status_code}",
+        )
+        self.assertTrue(
+            resp.json()["results"][0],
+            msg="Event not retrieved by attendants_name",
+        )
+
+        event_search_attendants_email = {
+            "search": event.attendants.first().email
+        }
+        resp = self.client.get(endpoint, event_search_attendants_email, format="json")
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_200_OK,
+            msg=f"Event not retrieved by attendants_email, status_code {resp.status_code}",
+        )
+        self.assertTrue(
+            resp.json()["results"][0],
+            msg="Event not retrieved by attendants_email",
+        )
+
+        event_search_notes = {
+            "search": event.notes
+        }
+        resp = self.client.get(endpoint, event_search_notes, format="json")
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_200_OK,
+            msg=f"Event not retrieved by notes, status_code {resp.status_code}",
+        )
+        self.assertTrue(
+            resp.json()["results"][0],
+            msg="Event not retrieved by notes",
+        )
+        event.delete()
+
+    def test_event_retrieve_by_date(self):
+        """[summary]
+        Test event retrieve by date endpoint.
+        [description]
+        Dates should be in YYYY-MM-DD and UTC.
+        """
+        event_title = "Test Event Creation"
+        event_data = self.event_data
+        event_data.update({
+            "title": event_title,
+            "attendants": [{"pid": str(self.contact.pid)}],
+        })
+        endpoint = reverse("events:create")
+        init_date = timezone.now()
+        resp = self.client.post(endpoint, event_data, format='json')
+        end_date = timezone.now()
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_201_CREATED,
+            msg=f"Event not created status code: {resp.status_code}",
+        )
+        event = Event.objects.get(title=event_title)
+        endpoint = reverse("events:items_date")
+
+        event_search_date = {
+            # Dates should be in YYYY-MM-DD and UTC.
+            "init": str(init_date).split()[0],
+            "end": str(end_date).split()[0],
+        }
+        resp = self.client.get(endpoint, event_search_date, format="json")
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_200_OK,
+            msg=f"Event not retrieved by date, status_code {resp.status_code}",
+        )
+        self.assertTrue(
+            resp.json()[0],
+            msg="Event not retrieved by date",
+        )
+        event.delete()
+
     def test_event_update(self):
         """[summary]
         Test event update endpoint with `patch`.
