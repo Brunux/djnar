f30ad6ebc3985e719fe14927745326cb78ef2eea
Bruno Granados <bruno.fosados@gmail.com>
2020-06-07 00:52:44 -0400

    :bug: Fix missing DRF settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -296,3 +296,14 @@ SOCIALACCOUNT_ADAPTER = (
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6,
+}
