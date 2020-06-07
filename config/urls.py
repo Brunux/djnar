7132c17f039e90922d608489e0816f8f49f18e49
Bruno Granados <bruno.fosados@gmail.com>
2020-06-07 00:08:32 -0400

    Update base project

--- config/urls.py
+++ config/urls.py
@@ -1,36 +1,58 @@
-"""djinar URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
-
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.views import defaults as default_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('c/', include(
-        ('djinar.contacts.urls', 'contacts'), namespace='contacts')
+    path(
+        "c/",
+        include(("djinar.contacts.urls", "contacts"), namespace="contacts"),
     ),
-    path('e/', include(
-        ('djinar.events.urls', 'events'), namespace='events')
+    path(
+        "e/",
+        include(("djinar.events.urls", "events"), namespace="events")
     ),
-    path('', include(('djinar.home.urls', 'home'), namespace='home')),
-]
+    path(
+        "",
+        include(("djinar.home.urls", "home"), namespace="home"),
+    ),
+    # Django Admin, use {% url 'admin:index' %}
+    path(settings.ADMIN_URL, admin.site.urls),
+    # User management
+    path(
+        "users/",
+        include("djinar.users.urls", namespace="users"),
+    ),
+    path("accounts/", include("allauth.urls")),
+    # Your stuff: custom urls includes go here
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    # This allows the error pages to be debugged during development, just visit
+    # these url in browser to see how these error pages look like.
+    urlpatterns += [
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
+    ]
+    if "debug_toolbar" in settings.INSTALLED_APPS:
+        import debug_toolbar
+
+        urlpatterns = [
+            path("__debug__/", include(debug_toolbar.urls))
+        ] + urlpatterns
